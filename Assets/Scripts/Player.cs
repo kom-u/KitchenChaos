a41728db3eb8000171ae814867313ee4619f3cdf
Atqa Munzir <atqamz@gmail.com>
2023-08-08 16:19:17 +0700

    feat: Character Visual, Move and Rotation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace KitchenChaos
+{
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private float moveSpeed = 5f;
+
+        private void Update()
+        {
+            var inputVector = new Vector2(0, 0);
+
+            if (Input.GetKey(KeyCode.W)) inputVector.y += 1;
+            if (Input.GetKey(KeyCode.A)) inputVector.x -= 1;
+            if (Input.GetKey(KeyCode.S)) inputVector.y -= 1;
+            if (Input.GetKey(KeyCode.D)) inputVector.x += 1;
+
+            inputVector = inputVector.normalized;
+
+            var moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+            var playerTransform = transform;
+            playerTransform.position += moveDir * (moveSpeed * Time.deltaTime);
+
+            var rotateSpeed = 5f;
+            playerTransform.forward = Vector3.Slerp(moveDir, playerTransform.forward, rotateSpeed * Time.deltaTime);
+        }
+    }
+}
