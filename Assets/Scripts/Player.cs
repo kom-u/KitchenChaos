6556f0cf31132bdb90581185c7d53207b565c7ce
Atqa Munzir <atqamz@gmail.com>
2023-08-09 10:42:26 +0700

    feat: Collision detection using Cast

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,14 +16,50 @@ namespace KitchenChaos {
         private void Update() {
             Vector2 inputVector = gameInput.GetMovementInputVector();
 
-            Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+            Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+            float moveDistance = moveSpeed * Time.deltaTime;
 
-            transform.position += moveDir * (moveSpeed * Time.deltaTime);
+            float playerRadius = 0.7f;
+            float playerHeight = 2f;
+            bool isCanMove = !Physics.CapsuleCast(
+                transform.position, // lower point
+                transform.position + Vector3.up * playerHeight, // upper point
+                playerRadius, // radius
+                moveDirection, // direction
+                moveDistance // distance
+            );
 
-            isWalking = moveDir != Vector3.zero;
+
+            if (!isCanMove) {
+                moveDirection = new Vector3(inputVector.x, 0f, 0f);
+                isCanMove = !Physics.CapsuleCast(
+                    transform.position, // lower point
+                    transform.position + Vector3.up * playerHeight, // upper point
+                    playerRadius, // radius
+                    moveDirection, // direction
+                    moveDistance // distance
+                );
+
+                if (!isCanMove) {
+                    moveDirection = new Vector3(0f, 0f, inputVector.y);
+                    isCanMove = !Physics.CapsuleCast(
+                        transform.position, // lower point
+                        transform.position + Vector3.up * playerHeight, // upper point
+                        playerRadius, // radius
+                        moveDirection, // direction
+                        moveDistance // distance
+                    );
+                }
+            }
+
+
+            if (isCanMove)
+                transform.position += moveDirection * moveDistance;
+
+            isWalking = moveDirection != Vector3.zero;
 
             float rotateSpeed = 5f;
-            transform.forward = Vector3.Slerp(moveDir, transform.forward, rotateSpeed * Time.deltaTime);
+            transform.forward = Vector3.Slerp(moveDirection, transform.forward, rotateSpeed * Time.deltaTime);
         }
     }
 }
