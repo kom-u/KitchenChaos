524eda9dfc13c6f5cc365ef8a855d5a9c03e4d4f
Atqa Munzir <atqamz@gmail.com>
2023-08-09 11:06:37 +0700

    feat: Adding first counter

---
+++ Assets/Scripts/ClearCounter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KitchenChaos {
+    public class ClearCounter : MonoBehaviour {
+        public void Interact() {
+            Debug.Log("ClearCounter.Interact()");
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,21 +3,30 @@ using UnityEngine;
 namespace KitchenChaos {
     public class Player : MonoBehaviour {
         [SerializeField] private GameInput gameInput;
+        [SerializeField] private LayerMask interactLayerMask;
 
         [SerializeField] private float moveSpeed = 5f;
         private bool isWalking;
-
         public bool IsWalking {
             get => isWalking;
         }
 
-
+        private Vector3 lastInteractDirection;
 
         private void Update() {
+            HandleMovement();
+            HandleInteraction();
+        }
+
+
+
+        // FIXME: refactor later
+        private void HandleMovement() {
             Vector2 inputVector = gameInput.GetMovementInputVector();
 
             Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
             float moveDistance = moveSpeed * Time.deltaTime;
+
 
             float playerRadius = 0.7f;
             float playerHeight = 2f;
@@ -60,6 +69,32 @@ namespace KitchenChaos {
 
             float rotateSpeed = 5f;
             transform.forward = Vector3.Slerp(moveDirection, transform.forward, rotateSpeed * Time.deltaTime);
+        }
+
+
+
+        private void HandleInteraction() {
+            Vector2 inputVector = gameInput.GetMovementInputVector();
+
+            Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+
+            if (moveDirection == Vector3.zero)
+                moveDirection = lastInteractDirection;
+
+            float interactDistance = 2f;
+            bool isCanInteract = Physics.Raycast(
+                transform.position,
+                lastInteractDirection,
+                out RaycastHit hitInfo,
+                interactDistance,
+                interactLayerMask
+            );
+
+            if (isCanInteract) {
+                if(hitInfo.transform.TryGetComponent(out ClearCounter clearCounter)){
+                    clearCounter.Interact();
+                }
+            }
         }
     }
 }
